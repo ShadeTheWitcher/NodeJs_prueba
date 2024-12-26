3222921683432e02f18f087f5b9e4c696f77820f
Matias Lovato <matii_seba_11@hotmail.com>
2024-12-26 15:15:26 -0300

    Login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -53,7 +53,33 @@ const register = async(req, res) => {
 // ruta   /api/v1/users/login
 const login = async(req, res) => {
     try {
-        
+        const {email, password} = req.body //pedimos el email y pass
+
+        if(!email || !password){ //validamos que exista la info solicitada
+            return res
+            .status(400)
+            .json({ error: "Missing required fields: email,password"});
+        }
+
+        const user = await UserModel.findOnebyEmail(email) //verif que exista email
+        if(!user){
+            return res.status(404).json({ error: "User not found"});
+        }
+
+        const isMatch = await bcryptjs.compare(password, user.password) //comparamos contraseña
+
+        if(!isMatch){
+            return res.status(401).json({ error: "Invalid credentials"});
+        }
+
+        const token = jwt.sign({email: user.email}, //generamos token con el payload donde solo colocamos el email, ya que es publico
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1h"
+            }
+        )
+        return res.json({ ok: true, msg: token})
+
     } catch (error) {
         console.log(error)
         return res.status(500).json({
