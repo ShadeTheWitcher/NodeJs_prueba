bb4eadf0f0031cc40946e283f05fa04507f8e545
Matias Lovato <matii_seba_11@hotmail.com>
2024-12-27 15:41:31 -0300

    Update user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,5 +6,6 @@ const router = Router()
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 
+router.get('/usuarios', UserController.login)
 
 export default router;
